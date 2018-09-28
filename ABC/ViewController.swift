cf893cc0da6805edf01f2cf2fe0b99ca6d51a1b0
20171105121 <s20171105121@mac24.local>
2018-09-28 08:40:13 +0800

    928

--- ABC/ViewController.swift
+++ ABC/ViewController.swift
@@ -9,7 +9,77 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var temp = 0;
 
+    @IBOutlet weak var caculaterDisplay: UITextField!
+    
+    @IBAction func botton1(_ sender: Any) {
+        caculaterDisplay.text =  caculaterDisplay.text!+"1"
+    }
+    
+    
+    @IBAction func botton2(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"2"
+    }
+    
+    
+    @IBAction func botton3(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"3"
+    }
+    
+    
+    @IBAction func button4(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"4"
+    }
+    
+    
+    @IBAction func button5(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"5"
+    }
+    
+ 
+    @IBAction func button6(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"6"
+    }
+    
+    @IBAction func button7(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"7"
+    }
+    
+    
+    @IBAction func button8(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"8"
+    }
+    
+    
+    @IBAction func button9(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"9"
+    }
+    
+    
+    @IBAction func button0(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"0"
+    }
+    
+    
+    @IBAction func buttonCaculator(_ sender: Any) {
+        var sum = 0;
+        sum = temp + Int(caculaterDisplay.text!)!
+        caculaterDisplay.text = "\(sum)"
+    }
+    
+    @IBAction func buttonAdd(_ sender: Any) {
+        temp =
+            Int(caculaterDisplay.text!)!
+        caculaterDisplay.text = ""
+       
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+        caculaterDisplay.text = ""
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
